0ca0fef92d34fc669a0ad20474da87f5dc5682b9
Ryland Adams <rylandadams@yahoo.com>
2022-11-19 15:55:55 -0800

    varibles, functions, methods
    Co-Authored-By: Thomas Mckay <mckay11.88@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 // THOMAS DAY 1
 
 // Varibles
+
+let numbers = 1;
+
+let string = 'string';
+
+let booleans = true; //or false
+
+let array = [];
+
+let object = {};
+
+// Methods
+
+let stringLenght = string.length;
+
+console.log(stringLenght);
+console.log(string);
+
+// Functions
+function add(param1, param2) {
+	return;
+}
+
+// Addition
+function add(num1, num2) {
+	let addAmount = num1 + num2;
+
+	return addAmount;
+}
+
+let catAge = 10;
+let dogAge = 15;
+
+let amountAge = add(catAge, dogAge);
+console.log(amountAge);
+
+// Subtraction
+
+function subtract(num1, num2) {
+	let subtractAmount = num1 - num2;
+
+	return subtractAmount;
+}
+
+let kidAge = 12;
+let adultAge = 45;
+
+let amountYears = subtract(kidAge, adultAge);
+console.log(amountAge);
